71e251f22099b0e804e45e2c88b7ef25e8e34f30
WedleySIlva <WedleySilva2105@gmail.com>
2023-08-31 13:25:24 -0300

    pastas desfeitas

--- innovar/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from .cliente_pacote import ClientePacote
-from .cliente_procedimento import ClienteProcedimento
-from .custom_user_manager import CustomUserManager
-from .horario_bloqueado import HorarioBloqueado
-from .pacote import Pacote
-from .procedimento import Procedimento
-from .usuario_customizado import UsuarioCustomizado
