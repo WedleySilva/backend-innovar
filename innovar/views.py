79b130359d0e0c9adc451e71d275f5a9f48b3084
Wedley Silva Schmoeller <99206531+WedleySilva@users.noreply.github.com>
2023-09-06 20:51:42 -0300

    comentando permission_classes

--- innovar/views.py
+++ innovar/views.py
@@ -13,25 +13,25 @@ from .serializers import (
 class UsuarioCustomizadoViewSet(viewsets.ModelViewSet):
     queryset = UsuarioCustomizado.objects.all()
     serializer_class = UsuarioCustomizadoSerializer
-    permission_classes = [IsAuthenticated]
+    # permission_classes = [IsAuthenticated]
 class ProcedimentoViewSet(viewsets.ModelViewSet):
     queryset = Procedimento.objects.all()
     serializer_class = ProcedimentoSerializer
-    permission_classes = [IsAuthenticated]
+    # permission_classes = [IsAuthenticated]
 class PacoteViewSet(viewsets.ModelViewSet):
     queryset = Pacote.objects.all()
     serializer_class = PacoteSerializer
-    permission_classes = [IsAuthenticated]
+    # permission_classes = [IsAuthenticated]
 class ClienteProcedimentoViewSet(viewsets.ModelViewSet):
     queryset = ClienteProcedimento.objects.all()
     serializer_class = ClienteProcedimentoSerializer
-    permission_classes = [IsAuthenticated]
+    # permission_classes = [IsAuthenticated]
 class ClientePacoteViewSet(viewsets.ModelViewSet):
     queryset = ClientePacote.objects.all()
     serializer_class = ClientePacoteSerializer
-    permission_classes = [IsAuthenticated]
+    # permission_classes = [IsAuthenticated]
 class HorarioBloqueadoViewSet(viewsets.ModelViewSet):
     queryset = HorarioBloqueado.objects.all()
     serializer_class = HorarioBloqueadoSerializer
-    permission_classes = [IsAuthenticated]
+    # permission_classes = [IsAuthenticated]
  
