8c767dd52dca3bc06a98a67e9935d5ed37a13b0d
WedleySilva <WedleySilva2105@gmail.com>
2023-09-11 15:02:40 -0300

    configurações de horarios bloqueados

--- innovar/models.py
+++ innovar/models.py
@@ -65,6 +65,23 @@ class HorarioBloqueado(models.Model):
     def __str__(self):
         return f"{self.dia} - {self.hora_inicio} às {self.hora_fim}"
 
+    def clean(self):
+        if self.hora_inicio >= self.hora_fim:
+            raise ValidationError("O horário de início deve ser menor que o horário de fim.")
+
+        existent_records = HorarioBloqueado.objects.filter(
+            dia=self.dia,
+            hora_inicio=self.hora_inicio,
+            hora_fim=self.hora_fim
+        ).exclude(pk=self.pk)  
+        if existent_records.exists():
+            raise ValidationError("Este horário já está bloqueado.")
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        super().save(*args, **kwargs)
+
+
 class Procedimento(models.Model):
     nome = models.CharField(max_length=255, unique=True)
     preco = models.DecimalField(max_digits=10, decimal_places=2)
