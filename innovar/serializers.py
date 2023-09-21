a6557939cae31821d140c56d3a90688079b12a58
WedleySIlva <WedleySilva2105@gmail.com>
2023-09-21 15:57:24 -0300

    validação de cpf e data de nascimento

--- innovar/serializers.py
+++ innovar/serializers.py
@@ -1,10 +1,23 @@
 from rest_framework import serializers
+from .validators import is_valid_cpf, is_valid_age
 from .models import UsuarioCustomizado, HorarioBloqueado, Procedimento, Pacote, ClienteProcedimento, ClientePacote, HorarioBloqueado
 
 class UsuarioCustomizadoSerializer(serializers.ModelSerializer):
     class Meta:
         model = UsuarioCustomizado
         fields = '__all__'
+
+    def validate_cpf(self, value):
+        # Implemente a validação personalizada do CPF aqui
+        if not is_valid_cpf(value):
+            raise serializers.ValidationError("CPF inválido")
+        return value
+
+    def validate_data_nascimento(self, value):
+        # Implemente a validação personalizada da data de nascimento aqui
+        if not is_valid_age(value):
+            raise serializers.ValidationError("Data de nascimento inválida")
+        return value
 
 class HorarioBloqueadoSerializer(serializers.ModelSerializer):
     class Meta:
--- innovar/views.py
+++ innovar/views.py
@@ -1,5 +1,6 @@
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
+from .validators import is_valid_cpf, is_valid_age
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import UsuarioCustomizadoSerializer
@@ -20,9 +21,36 @@ def cadastrar_usuario(request):
     if request.method == 'POST':
         serializer = UsuarioCustomizadoSerializer(data=request.data)
         if serializer.is_valid():
+            # Antes de salvar o usuário, você pode adicionar sua lógica de validação personalizada aqui
+
+            # Verifique se o CPF é válido (você pode usar uma função personalizada para isso)
+            cpf = serializer.validated_data.get('cpf')
+            if not is_valid_cpf(cpf):
+                return Response({'cpf': 'CPF inválido.'}, status=status.HTTP_400_BAD_REQUEST)
+
+            # Verifique a idade com base na data de nascimento
+            data_nascimento = serializer.validated_data.get('data_nascimento')
+            if not is_valid_age(data_nascimento):
+                return Response({'data_nascimento': 'Data de nascimento inválida.'}, status=status.HTTP_400_BAD_REQUEST)
+
+            # Outras validações personalizadas podem ser adicionadas aqui
+
+            # Após a validação personalizada, salve o usuário
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# Função para validar o CPF (você pode usar uma biblioteca como "validate-docbr")
+def is_valid_cpf(cpf):
+    # Implemente a lógica para validar o CPF aqui
+    # Retorna True se o CPF for válido, False caso contrário
+    return True  # Substitua por sua própria lógica de validação de CPF
+
+# Função para verificar a idade com base na data de nascimento
+def is_valid_age(data_nascimento):
+    # Implemente a lógica para verificar a idade aqui
+    # Retorna True se a idade for válida, False caso contrário
+    return True  # Substitua por sua própria lógica de validação de idade
 
 class UsuarioCustomizadoViewSet(viewsets.ModelViewSet):
     queryset = UsuarioCustomizado.objects.all()
