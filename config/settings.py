db69408e14f799e92240f57cd299a6c1a7eae690
WedleySIlva <WedleySilva2105@gmail.com>
2023-10-23 15:08:43 -0300

    Falta configuração e teste do Clounary

--- config/settings.py
+++ config/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
+    'cloudinary',
     'innovar',
     'django_extensions',
     'rest_framework',
@@ -204,4 +206,14 @@ else:
 
 print(MODE, DATABASES)
 
+STATIC_URL = "/static/"
 
+if MODE in ["PRODUCTION", "MIGRATE"]:
+    CLOUDINARY_URL = os.getenv("CLOUDINARY_URL")
+    DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+    STATIC_ROOT = os.path.join(BASE_DIR, "static")
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    MEDIA_URL = '/media/' 
+else:    
+    MY_IP = os.getenv("MY_IP", "127.0.0.1")
+    MEDIA_URL = f"http://{MY_IP}:19003/media/"
